b1ef1189d8bf4d0d367de5fb573745e78c2953a6
Barış Can Çifçi <78167969+Bariscncfc@users.noreply.github.com>
2022-04-11 14:47:50 +0300

    Hero Section
    Hero section is added

--- daillo/src/components/Hero/index.js
+++ daillo/src/components/Hero/index.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import { HeroBg, HeroContainer, VideoBg } from './HeroElements'
+import React, { useState } from 'react'
+import { ArrowForward, ArrowRight, HeroBg, HeroBtnWrapper, HeroContainer, HeroContent, HeroH1, HeroP, VideoBg } from './HeroElements'
 import Teaser from '../../videos/Teaser.mp4'
+import { Button } from '../Button';
 
 const Hero = () => {
+
+    const [hover,setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    }
+
   return (
     <div>
        <HeroContainer id="home">
            <HeroBg>
                <VideoBg autoPlay loop muted src={Teaser} type='video/mp4' />
            </HeroBg>
+           <HeroContent>
+               <HeroH1>GET A DAILY ENGLISH STORY</HeroH1>
+               <HeroP>Subscribe to the application by e-mail</HeroP>
+               <HeroBtnWrapper>
+                   <Button onMouseEnter={onHover} onMouseLeave={onHover} to="/">Subscribe {hover ? <ArrowForward /> : <ArrowRight />}</Button>
+               </HeroBtnWrapper>
+           </HeroContent>
        </HeroContainer> 
     </div>
   )
